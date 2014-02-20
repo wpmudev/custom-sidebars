e277f722d39c66bbfef08a803c8c3fc139251a3e
aaron <aaron@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-02-20 03:38:27 +0000

    Fix error notices with BuddyPress Fix compatibility issues with other plugin/theme updaters Move cron update check to run independent of user logged in
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/wpmudev-dashboard-notification@12937 f53fe46e-8c02-459b-9fdd-93e75771b278

--- wpmudev-dash-notification.php
+++ wpmudev-dash-notification.php
@@ -13,22 +13,25 @@ if ( !class_exists('WPMUDEV_Dashboard_Notice3') ) {
 		var $update_count = 0;
 		
 		function __construct() {
-			add_action( 'plugins_loaded', array( &$this, 'init' ) );
+			add_action( 'init', array( &$this, 'init' ) );
 		}
 		
 		function init() {
 			global $wpmudev_un;
+			
+			if ( class_exists( 'WPMUDEV_Dashboard' ) || ( isset($wpmudev_un->version) && version_compare($wpmudev_un->version, '3.4', '<') ) )
+				return;
+			
+			// Schedule update jobs
+			if ( !wp_next_scheduled('wpmudev_scheduled_jobs') ) {
+				wp_schedule_event(time(), 'twicedaily', 'wpmudev_scheduled_jobs');
+			}
+			add_action( 'wpmudev_scheduled_jobs', array( $this, 'updates_check') );
+			add_action( 'delete_site_transient_update_plugins', array( &$this, 'updates_check' ) ); //refresh after upgrade/install
+			add_action( 'delete_site_transient_update_themes', array( &$this, 'updates_check' ) ); //refresh after upgrade/install
+			
 			if ( is_admin() && current_user_can( 'install_plugins' ) ) {
-				if ( class_exists( 'WPMUDEV_Dashboard' ) || ( isset($wpmudev_un->version) && version_compare($wpmudev_un->version, '3.4', '<') ) )
-					return;
 				
-				// Schedule update jobs
-				if ( !wp_next_scheduled('wpmudev_scheduled_jobs') ) {
-					wp_schedule_event(time(), 'twicedaily', 'wpmudev_scheduled_jobs');
-				}
-				add_action( 'wpmudev_scheduled_jobs', array( $this, 'updates_check') );
-				add_action( 'delete_site_transient_update_plugins', array( &$this, 'updates_check' ) ); //refresh after upgrade/install
-				add_action( 'delete_site_transient_update_themes', array( &$this, 'updates_check' ) ); //refresh after upgrade/install
 				add_action( 'site_transient_update_plugins', array( &$this, 'filter_plugin_count' ) );
 				add_action( 'site_transient_update_themes', array( &$this, 'filter_theme_count' ) );
 				add_filter( 'plugins_api', array( &$this, 'filter_plugin_info' ), 20, 3 ); //run later to work with bad autoupdate plugins
@@ -478,7 +481,7 @@ if ( !class_exists('WPMUDEV_Dashboard_Notice3') ) {
 				return $res;
 			}
 	
-			return false;
+			return $res;
 		}
 		
 		function filter_plugin_rows() {
