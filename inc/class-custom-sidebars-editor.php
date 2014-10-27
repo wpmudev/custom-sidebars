21d7e28a45bebb59fe71416a28354acc034d254e
Philipp Stracker <philipp@incsub.com>
2014-10-27 21:46:04 +0100

    Fix error "undefined function mb_strlen"
    The mbstring extension is non-default and may not be enabled on every
    server. If it is not enabled we assume that the server supports latin
    characters only (1-byte characters)

--- inc/class-custom-sidebars-editor.php
+++ inc/class-custom-sidebars-editor.php
@@ -179,8 +179,13 @@ class CustomSidebarsEditor extends CustomSidebars {
 	private function save_item( $req, $data )  {
 		$sidebars = self::get_custom_sidebars();
 		$sb_id = $req->id;
-		$sb_name = mb_substr( stripslashes( trim( @$data['name'] ) ), 0, 40 );
 		$sb_desc = stripslashes( trim( @$_POST['description'] ) );
+
+		if ( function_exists( 'mb_substr' ) ) {
+			$sb_name = mb_substr( stripslashes( trim( @$data['name'] ) ), 0, 40 );
+		} else {
+			$sb_name = substr( stripslashes( trim( @$data['name'] ) ), 0, 40 );
+		}
 
 		if ( empty( $sb_name ) ) {
 			return self::req_err(
@@ -214,8 +219,14 @@ class CustomSidebarsEditor extends CustomSidebars {
 			}
 		}
 
-		if ( mb_strlen( $sb_desc ) > 200 ) {
-			$sb_desc = mb_substr( $sb_desc, 0, 200 );
+		if ( function_exists( 'mb_strlen' ) ) {
+			if ( mb_strlen( $sb_desc ) > 200 ) {
+				$sb_desc = mb_substr( $sb_desc, 0, 200 );
+			}
+		} else {
+			if ( strlen( $sb_desc ) > 200 ) {
+				$sb_desc = substr( $sb_desc, 0, 200 );
+			}
 		}
 
 		// Populate the sidebar object.
