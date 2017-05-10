bdea6c3695978bbf497c9179c089107e4ac160da
Marcin Pietrzak <marcin@iworks.pl>
2017-05-10 13:54:04 +0200

    [Custom Sidebars] replace sidebar on archive 404 page not working https://app.asana.com/0/20298069742332/63670530301499

--- inc/class-custom-sidebars-replacer.php
+++ inc/class-custom-sidebars-replacer.php
@@ -126,7 +126,10 @@ class CustomSidebarsReplacer extends CustomSidebars {
 		// Legacy handler with camelCase
 		do_action( 'cs_predetermineReplacements', $defaults );
 
-		$replacements = $this->determine_replacements( $defaults );
+        $replacements = $this->determine_replacements( $defaults );
+
+//l($replacements);
+
 
 		foreach ( $replacements as $sb_id => $replace_info ) {
 			if ( ! is_array( $replace_info ) || count( $replace_info ) < 3 ) {
@@ -313,7 +316,7 @@ class CustomSidebarsReplacer extends CustomSidebars {
 					);
 				}
 			}
-		} elseif ( ! is_category() && ! is_singular() && get_post_type() != 'post' ) {
+		} elseif ( ! is_404() && ! is_category() && ! is_singular() && get_post_type() != 'post' ) {
 			// 4 |== Post-Tpe Archive ----------------------------------------------
 			// `get_post_type() != 'post'` .. post-archive = post-index (see 7)
 
@@ -540,7 +543,7 @@ class CustomSidebarsReplacer extends CustomSidebars {
 			}
 		} elseif ( is_404() ) {
 			// 11 |== 404 not found ------------------------------------------------
-
+l('404');
 			$expl && do_action( 'cs_explain', 'Type 11: 404 not found' );
 
 			foreach ( $sidebars as $sb_id ) {
