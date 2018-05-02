974a42c8177aaeb996b157bc4273d06cd1a17d94
Marcin Pietrzak <marcin@iworks.pl>
2018-05-02 07:40:24 +0200

    prepare 3.1.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function( grunt ) {
 				'!.git',
 				'!.log',
 			],
-			base: 'development',
+			base: 'master',
 			pro: 'customsidebars-pro',
 			free: 'customsidebars-free',
 		},
