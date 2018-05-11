70040249ef948ccba5e98a7c910a2e6e44b2c584
Marcin Pietrzak <marcin@iworks.pl>
2018-05-11 09:20:04 +0200

    prepare 3.1.6

--- assets/js/cs-cloning.js
+++ assets/js/cs-cloning.js
@@ -1,4 +1,4 @@
-/*! Custom Sidebars - v3.1.5
+/*! Custom Sidebars - v3.1.6-beta.1
  * https://premium.wpmudev.org/project/custom-sidebars-pro/
  * Copyright (c) 2018; * Licensed GPLv2+ */
 /*global jQuery:false */
